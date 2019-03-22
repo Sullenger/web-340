e452fdb458181051ab20992516e7b4b8831fa4b2
Jason Sullenger <jssullenger@my365.bellevue.edu>
2019-03-21 20:47:25 -0600

    adding pug-Template

--- week-5/pug-Template/sullenger-Exercise-5.3.js
+++ week-5/pug-Template/sullenger-Exercise-5.3.js
@@ -1,35 +1,33 @@
 /*
 ============================================
-; Title:  sullenger-Exercise-5.2.js
+; Title:  sullenger-Exercise-5.3.js
 ; Author: Jason Sullenger
 ; Date:   21 March 2019
-; Description:rendering array to HTML with ejs if/else
+; Description: Rendering HTML with pug-Template
 ;===========================================
 */
 
 // Console logs the header created previously
 const header = require('../../sullenger-header.js');
-console.log(header.display("Jason" , "Sullenger" , "if-Else-Render"));
+console.log(header.display("Jason" , "Sullenger" , "pug-Template"));
 
 var express = require("express");
 var http = require("http");
+var pug = require("pug");
 var path = require("path");
 
 var app = express();
 
 app.set("views", path.resolve(__dirname, "views"));
 
-app.set("view engine", "ejs");
+app.set("view engine", "pug");
 
-var names = ["Jack", "Jimmy", "Janet", "Jeremy", "Jason", "James"];
-
-app.get("/", function(req, res){
-  res.render("index.ejs",{
-    names:names
-  });
-});
+app.get("/", function(req,res){
+  res.render("index",{
+    message: "First attempt PUG homepage"
+  })
+})
 
 http.createServer(app).listen(8080, function(){
   console.log("We are live on 8080!")
-});
-
+})

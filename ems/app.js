1f7e73510cd98b4fbefb480417800870f5e79914
Jason Sullenger <jssullenger@my365.bellevue.edu>
2019-03-31 15:12:04 -0600

    updating milestone 2

--- ems/app.js
+++ ems/app.js
@@ -8,7 +8,7 @@
 */
 
 // Console logs the header created previously
-const header = require("../sullenger-header.js");
+const header = require("../sullenger-header");
 console.log(header.display("Jason", "Sullenger", "EMS"));
 
 var express = require("express");
@@ -22,14 +22,20 @@ app.set("views", path.resolve(__dirname, "views/"));
 
 app.set("view engine", "ejs");
 
+// Logging Morgan
 app.use(logger("short"));
 
+// Styling fix from Overstack
+app.use(express.static(__dirname + '/public'));
+
+// Routing
 app.get("/", function(req, res) {
   res.render("index", {
     title: "Homepage"
   });
 });
 
+// Creates app
 http.createServer(app).listen(8080, function() {
   console.log("We are live on 8080!");
 });

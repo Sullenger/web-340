21e5539c4eb5ec41515d7bba06289e2e7c429855
Jason Sullenger <jssullenger@my365.bellevue.edu>
2019-04-11 22:44:55 -0600

    adding csrf protection

--- ems/app.js
+++ ems/app.js
@@ -18,8 +18,17 @@ var path = require("path");
 var mongoose = require("mongoose");
 var logger = require("morgan");
 var helmet = require("helmet")
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var csrf = require("csurf");
 
 var Employee = require("./models/employee.js");
+
+// CSRF setup
+var csrfProtection = csrf({cookie:true});
+
+var app = express();
+
 
 // connection to mLab
 var mongoDB = "mongodb+srv://user_208:bznXR3on@ems-nhomg.mongodb.net/test?retryWrites=true";
@@ -38,16 +47,30 @@ db.once("open", function(){
   console.log("Application connected to mLab MongoDB instance")
 })
 
-var app = express();
+
+app.use(helmet.xssFilter());
 
 app.use(logger("short"));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(cookieParser());
+
+app.use(csrfProtection);
+
+app.use(function(req, res, next){
+  var token = req.csrfToken();
+  res.cookie('XSRF-TOKEN', token);
+  res.locals.csrfToken = token;
+  next();
+});
 
 var employee = new Employee({
   firstName: "John",
   lastName: "Smith"
 })
-
-app.use(helmet.xssFilter());
 
 app.set("views", path.resolve(__dirname, "views/"));
 
@@ -65,6 +88,17 @@ app.get("/", function(req, res) {
     title: "Homepage",
   });
 });
+
+app.get("/", function(req, res){
+  res.render("index", {
+    message: "New Employee"
+  })
+})
+
+app.post("/process", function(req, res){
+  console.log(req.body.txtName);
+  res.redirect("/");
+})
 
 // Creates app
 http.createServer(app).listen(8080, function() {
